269d9a374afe063c8f77381c2306c96d9a78c385
Tima Baisalov <baisalov.t98@gmail.com>
2024-03-26 10:22:11 +0600

    count increment added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  function increment() {
+    setCount(count + 1)
+  }
+  function decrement() {
+    setCount(count - 1)
+  }
   return (
     <div>
-      
+      {count} <br />
+      <button onClick={() => decrement()}>Decrement</button>
+      <button onClick={() => increment()}>Increment</button>
     </div>
   );
 }
