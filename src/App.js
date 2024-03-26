92301a0e0207c547eaa433c722f314d54bedc69c
Tima Baisalov <baisalov.t98@gmail.com>
2024-03-26 11:14:20 +0600

    posts displayed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import PostForm from "./components/PostForm";
 
 function App() {
+  const [data, setData] = useState([]);
+
+  fetch("https://jsonplaceholder.typicode.com/posts?_limit=10")
+    .then((res) => res.json())
+    .then((info) => setData(info));
+
   return (
-      <div>
-        <PostForm/>
-      </div>
-  )
+    <div>
+      {data.map((postData) => (
+        <div>
+          <h3>{postData.title}</h3>
+          <p>{postData.body}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
