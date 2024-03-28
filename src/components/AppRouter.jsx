da481c6e7626583a008d69473ba0863acfaec3e0
Tima Baisalov <baisalov.t98@gmail.com>
2024-03-28 12:15:37 +0600

    postIdPage added

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Posts from "../pages/Posts";
 import About from "../pages/About";
+import PostIdPage from "../pages/PostIdPage";
+import Error from "../pages/Error";
 
 const AppRouter = () => {
   return (
     <Routes>
       <Route path="/posts" element={<Posts />} />
       <Route path="/about" element={<About />} />
+      <Route path="/posts/:id" element={<PostIdPage />} />
+      <Route path="/error" element={<Error />} />
     </Routes>
   );
 };
