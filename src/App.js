9b2fefe62979d92bb2c7e03d4f463bb0663c44d6
Tima Baisalov <baisalov.t98@gmail.com>
2024-03-28 11:45:47 +0600

    unnecessary paths deleted

--- src/App.js
+++ src/App.js
@@ -1,15 +1,3 @@
-// import { useState, useEffect } from "react";
-// import PostList from "./components/PostList";
-// import PostForm from "./components/PostForm";
-// import PostFilter from "./components/PostFilter";
-// import MyModal from "./components/UI/MyModal/MyModal";
-// import MyButton from "./components/UI/button/MyButton";
-// import { usePosts } from "./hooks/usePosts";
-// import PostService from "./API/PostService";
-// import Loader from "./components/UI/Loader/Loader";
-// import { useFetching } from "./hooks/useFetching";
-// import Pagination from "./components/UI/pagination/Pagination";
-// import { getPageCount } from "./utils/pages";
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import About from "./pages/About";
