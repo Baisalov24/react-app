19fa264df3f71a7f21b8b8e9785f73ac28156540
Tima Baisalov <baisalov.t98@gmail.com>
2024-03-28 12:24:29 +0600

    route optimization

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,17 +1,13 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Posts from "../pages/Posts";
-import About from "../pages/About";
-import PostIdPage from "../pages/PostIdPage";
-import Error from "../pages/Error";
+import { routes } from "../router";
 
 const AppRouter = () => {
   return (
     <Routes>
-      <Route path="/posts" element={<Posts />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/posts/:id" element={<PostIdPage />} />
-      <Route path="/error" element={<Error />} />
+      {routes.map((route) => (
+        <Route path={route.path} element={<route.component />} />
+      ))}
     </Routes>
   );
 };
