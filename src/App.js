f33caa3b48f02658287582f2d7c54827a948218a
Tima Baisalov <baisalov.t98@gmail.com>
2024-03-28 11:44:18 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -14,15 +14,16 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import About from "./pages/About";
 import Posts from "./pages/Posts";
+import Navbar from "./components/UI/Navbar/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="" element={<Posts/>}/>
-      <Route path="/about" element={<About/>}/>
-    </Routes>
-     
+      <Navbar />
+      <Routes>
+        <Route path="/posts" element={<Posts />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
     </BrowserRouter>
   );
 }
