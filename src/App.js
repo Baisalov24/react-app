782ebf51860d388729b0f17bac18217a4b274e12
Tima Baisalov <baisalov.t98@gmail.com>
2024-03-28 08:11:01 +0600

    modal added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
 import MySelect from "./components/UI/select/MySelect";
 import PostFilter from "./components/PostFilter";
+import MyModal from "./components/UI/MyModal/MyModal";
+import MyButton from "./components/UI/button/MyButton";
 
 function App() {
   const [posts, setPosts] = useState([]);
@@ -14,34 +16,48 @@ function App() {
       .then((info) => setPosts(info));
   }, []);
 
-  const [filter, setFilter] = useState({sort: "", query: ""})
+  const [filter, setFilter] = useState({ sort: "", query: "" });
+  const [modal, setModal] = useState(false);
 
   const sortedPosts = useMemo(() => {
-    if(filter.sort) {
-      return [...posts].sort((a,b) => a[filter.sort].localeCompare(b[filter.sort]))
+    if (filter.sort) {
+      return [...posts].sort((a, b) =>
+        a[filter.sort].localeCompare(b[filter.sort])
+      );
     }
     return posts;
-  }, [filter.sort, posts])
+  }, [filter.sort, posts]);
 
   const sortedAndSeacrchedPosts = useMemo(() => {
-    return sortedPosts.filter(post => post.title.toLowerCase().includes(filter.query.toLowerCase()))
-  }, [filter.query, sortedPosts])
+    return sortedPosts.filter((post) =>
+      post.title.toLowerCase().includes(filter.query.toLowerCase())
+    );
+  }, [filter.query, sortedPosts]);
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
+    setModal(false)
   };
 
   function removePost(post) {
     setPosts(posts.filter((p) => p.id !== post.id));
   }
 
- 
   return (
     <div className="App">
-      <PostForm create={createPost} />
+      <MyButton onClick = {() => setModal(true)}>
+        Click
+      </MyButton>
+      <MyModal visible={modal} setVisible={setModal}>
+        <PostForm create={createPost} />{" "}
+      </MyModal>
       <hr style={{ margin: "15px 0" }} />
       <PostFilter filter={filter} setFilter={setFilter} />
-      <PostList posts={sortedAndSeacrchedPosts} title={"List of Posts"} remove={removePost} />
+      <PostList
+        posts={sortedAndSeacrchedPosts}
+        title={"List of Posts"}
+        remove={removePost}
+      />
     </div>
   );
 }
